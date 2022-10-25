b27ca2d7eb2bc138ba3a105b00121782a21da7d2
Bonou <784657357@qq.com>
2022-10-25 18:04:13 +0800

    剑指offer 之前的题目新提交

--- leetcodeSt/src/main/leetcode/editor/cn/P剑指 Offer 25_HeBingLiangGePaiXuDeLianBiaoLcof.java
+++ leetcodeSt/src/main/leetcode/editor/cn/P剑指 Offer 25_HeBingLiangGePaiXuDeLianBiaoLcof.java
@@ -34,7 +34,7 @@ class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         ListNode dummy = new ListNode(-1);
         ListNode pre = dummy;
-        // 取l1、l2共同的长度部分比较大小
+        // 取l1、l2共同的长度部分比较大小，l1 l2长度可能不一样
         while(l1 != null && l2 != null){
             if(l1.val <= l2.val){
                 pre.next = l1;
